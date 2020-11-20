c1e2241b956eddb043903bfe35871efd711545df
Lander Van laer <lander.vanlaer1@gmail.com>
2020-11-20 17:48:04 +0100

    Oefeningen les 20 november

---
+++ assignments/php/antwoordpost2.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        if (!isset($_POST["voornaam"]) || !isset($_POST["achternaam"]))
+            echo "Error, u kan deze pagina niet bereiken";
+        else
+            echo "Hallo {$_POST["voornaam"]} {$_POST["achternaam"]}<br>";
+    ?>
+    <br>
+    <a href="formpost.html">Ga terug</a>
+</body>
+</html>
